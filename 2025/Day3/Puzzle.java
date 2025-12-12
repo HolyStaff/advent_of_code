a1f58f04bba9ccd77c101c2880535e25e0c39756
HolyStaff <h9dead@gmail.com>
2025-12-12 22:38:30 +0100

    idk stuff

--- 2025/Day3/Puzzle.java
+++ 2025/Day3/Puzzle.java
@@ -1,24 +1,17 @@
 package Day3;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Puzzle {
-    public static void main(String[] args) {
-        File myFile = new File("input.txt");
+    public static void main(String[] args) throws Exception {
+        File myFile = new File("test.txt");
 
         try (Scanner myReader = new Scanner(myFile)) {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.print("bank: ");
-                System.out.println(data);
-                int check = checkLargestNumber(data);
-                System.out.println(check);
+                checkLargestNumber(data);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
         }
     }
 
@@ -26,10 +19,6 @@ public class Puzzle {
         int largest_number = 0;
         for (char index : battery_bank.toCharArray()) {
             System.out.println(index);
-            if (index > largest_number){
-                largest_number = index;
-                System.out.println(largest_number);
-            }
         }
         return largest_number;
     }
